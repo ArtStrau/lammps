31d2026a7fab4dcab71e19a997a3130bc2327b15
Arthur Straube <straube@zib.de>
2025-12-20 01:51:21 +0100

    BROWNIAN: Add Tp_ROTGEOM template parameter to fix brownian/sphere

--- src/BROWNIAN/fix_brownian_sphere.cpp
+++ src/BROWNIAN/fix_brownian_sphere.cpp
@@ -61,34 +61,34 @@ void FixBrownianSphere::initial_integrate(int /*vflag */)
 {
   if (domain->dimension == 2) {
     if (!noise_flag) {
-      initial_integrate_templated<0, 0, 1, 0>();
+      initial_integrate_templated<0, 0, 0, 1, 0>();
     } else if (gaussian_noise_flag) {
-      initial_integrate_templated<0, 1, 1, 0>();
+      initial_integrate_templated<0, 0, 1, 1, 0>();
     } else {
-      initial_integrate_templated<1, 0, 1, 0>();
+      initial_integrate_templated<0, 1, 0, 1, 0>();
     }
   } else if (planar_rot_flag) {
     if (!noise_flag) {
-      initial_integrate_templated<0, 0, 0, 1>();
+      initial_integrate_templated<0, 0, 0, 0, 1>();
     } else if (gaussian_noise_flag) {
-      initial_integrate_templated<0, 1, 0, 1>();
+      initial_integrate_templated<0, 0, 1, 0, 1>();
     } else {
-      initial_integrate_templated<1, 0, 0, 1>();
+      initial_integrate_templated<0, 1, 0, 0, 1>();
     }
   } else {
     if (!noise_flag) {
-      initial_integrate_templated<0, 0, 0, 0>();
+      initial_integrate_templated<0, 0, 0, 0, 0>();
     } else if (gaussian_noise_flag) {
-      initial_integrate_templated<0, 1, 0, 0>();
+      initial_integrate_templated<0, 0, 1, 0, 0>();
     } else {
-      initial_integrate_templated<1, 0, 0, 0>();
+      initial_integrate_templated<0, 1, 0, 0, 0>();
     }
   }
 }
 
 /* ---------------------------------------------------------------------- */
 
-template <int Tp_UNIFORM, int Tp_GAUSS, int Tp_2D, int Tp_2Drot>
+template <int Tp_ROTGEOM, int Tp_UNIFORM, int Tp_GAUSS, int Tp_2D, int Tp_2Drot>
 void FixBrownianSphere::initial_integrate_templated()
 {
   double **x = atom->x;
